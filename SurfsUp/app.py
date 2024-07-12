9451aee36f4908ccda5766c6547f2fc2d4bd5a1c
dtjskim <david.js.kim@outlook.com>
2024-07-11 22:23:47 -0700

    app.py file updated

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -13,7 +13,7 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -21,7 +21,8 @@ Base = automap_base()
 Base.prepare(autoload_with=engine)
 
 # Save references to each table
-Passenger = Base.classes.passenger
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 
@@ -36,3 +37,64 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all precipitation
+    results = session.query(Measurement.date, Measurement.prcp).all()
+
+    session.close()
+
+    # Convert list of tuples into dictionary
+    all_precipitation = {date: prcp for date, prcp in results}
+
+    return jsonify(all_precipitation)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all stations
+    results = session.query(Station.station, Station.name).all()
+
+    session.close()
+
+    # Convert list of tuples into dictionary
+    all_stations = {station: name for station, name in results}
+
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query the dates and temperature observations of the most active station for the last year of data
+    # Assuming the most active station is USC00519281
+    results = session.query(Measurement.date, Measurement.tobs)\
+                     .filter(Measurement.station == 'USC00519281')\
+                     .filter(Measurement.date >= '2016-08-23')\
+                     .all()
+
+    session.close()
+
+    # Convert list of tuples into list of dictionaries
+    all_tobs = [{date: tobs} for date, tobs in results]
+
+    return jsonify(all_tobs)
+
+if __name__ == '__main__':
+    app.run(debug=True)
